9f6b6db6f046fd2695a6c2cbab8bc645d8249830
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-06-29 01:49:11 +0100

    feat/code-setup-db-migration

---
+++ tasks/models.py
@@ -0,0 +1,25 @@
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+        ('Overdue', 'Overdue'),
+    ]
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    due_date = models.DateTimeField()
+    category = models.CharField(max_length=255)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
