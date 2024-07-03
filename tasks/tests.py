4a462db93f72945c035bbffe5c2594cd6ab1ad6d
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-07-03 09:57:16 +0100

    feat/add-tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.task = Task.objects.create(
+            title='Test Task',
+            description='Test Description',
+            status='In Progress',
+            priority='Medium',
+            due_date='2024-07-01 10:00',
+            category='Development',
+            assigned_to=self.user
+        )
+
+    def test_task_creation(self):
+        self.assertEqual(self.task.title, 'Test Task')
+
+    def test_dashboard_view(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
